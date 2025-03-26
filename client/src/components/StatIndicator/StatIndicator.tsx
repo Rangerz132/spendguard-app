7fae2c407f359e0cb617b43f670713659143fe6f
Anthony Trépanier <anthony.trepanier@hotmail.com>
2025-03-26 10:24:19 -0400

    initial-commit

---
+++ client/src/components/StatIndicator/StatIndicator.tsx
@@ -0,0 +1,19 @@
+import { BiUpArrowAlt } from "react-icons/bi";
+const StatIndicator = (props: { percValue: number; isIncreasing: boolean }) => {
+  return (
+    <div
+      className={`flex flex-row justify-center items-center px-2 py-1 space-x-1 rounded-md ${
+        props.isIncreasing ? "bg-lime/50" : "bg-cherry/50"
+      }`}
+    >
+      <BiUpArrowAlt
+        className={props.isIncreasing ? "text-lime" : "text-cherry rotate-180"}
+      />
+      <p className={`${props.isIncreasing ? "text-lime" : "text-cherry"}`}>
+        {props.percValue}%
+      </p>
+    </div>
+  );
+};
+
+export default StatIndicator;
